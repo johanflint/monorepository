25a538f037a76875281a9097ece63c7e41b144bc
Johan Flint <johan.flint@gmail.com>
2020-09-24 11:52:05 +0200

    Use lib

--- alfred.kts
+++ alfred.kts
@@ -1,2 +1,8 @@
-println("Hello world")
-println("2")
+println("Hello word")
+log("Johan", mapOf("command" to "git log", "exitCode" to 23))
+
+dir("monorepository") {
+    val x = sh("git log -1").block()
+    log("Git: $x")
+}
+
